e1c4077a6109fb8d28b54dc74de853ee90bdaebc
alkali333 <jake@alkalimedia.co.uk>
2024-09-12 17:52:08 +0700

    Replaced morning practice

--- app/(dashboard)/morning-practice/page.js
+++ app/(dashboard)/morning-practice/page.js
@@ -1,64 +1,45 @@
 "use client";
 import { useState, useEffect } from "react";
-import { FaCheckCircle } from "react-icons/fa";
-import { FaSun } from "react-icons/fa";
-import toast from "react-hot-toast";
+import { FaMoon } from "react-icons/fa";
+import { FormContainer } from "/app/components/forms/FormContainer";
+import { fetchUserJson } from "../../utils/server-actions";
+import DiaryInputV2 from "/app/components/forms/DiaryInputV2";
+import DetailDisplay from "../../components/DetailDisplay";
 
 import {
-  updateMorningJournal,
-  fetchCoachingContent,
-} from "../../utils/server-actions";
-import FormContainer from "/app/components/forms/FormContainer";
-import DailyInputForm from "/app/components/forms/DailyInputForm";
+  insertDiaryEntry,
+  generateMorningPracticeMessage,
+} from "/app/utils/server-actions";
+
 import Meditation from "/app/components/Meditation";
 
-const MorningPractice = () => {
-  const gratitudeItems = [
-    { name: "gratitude1", placeholder: "One" },
-    { name: "gratitude2", placeholder: "Two" },
-    { name: "gratitude3", placeholder: "Three" },
-    { name: "gratitude4", placeholder: "Four" },
-    { name: "gratitude5", placeholder: "Five" },
-  ];
-
-  const toDoItems = [
-    { name: "todo1", placeholder: "One" },
-    { name: "todo2", placeholder: "Two" },
-    { name: "todo3", placeholder: "Three" },
-    { name: "todo4", placeholder: "Four" },
-    { name: "todo5", placeholder: "Five" },
-  ];
-
-  const [gratitudeComplete, setGratitudeComplete] = useState(false);
-  const [toDoComplete, setToDoComplete] = useState(false);
+const EveningPracticePage = () => {
+  const [journalComplete, setJournalComplete] = useState(false);
+  // const [userJson, setUserJson] = useState(null);
   const [encouragementMessage, setEncouragementMessage] = useState(null);
 
-  const formsComplete = gratitudeComplete && toDoComplete;
+  const formsComplete = journalComplete;
 
   useEffect(() => {
-    const getEncouragementMessage = async () => {
-      const tempEncouragementMessage =
-        await fetchCoachingContent(`Based on the USER INFO. Write a short message
-      (125 words) reminding them of their goals and the importance of their morning practice.
-      Invite them to record their "daily gratitude" and task list (this is not just a
-      regular 'to do' list, it is a list of what they want to achieve, or how they want to feel
-      today - explain and give examples of some feelings). 
-      
-      ONLY If there are existing task & gratitude
-      lists invite them to remember these and ask if they are still relevant.
-      Encourage them to be creative and think big.`);
+    // const getUserJson = async () => {
+    //   const userJson = await fetchUserJson();
+    //   const firstName = Object.keys(userJson)[0];
+    //   const typeJson = userJson[firstName];
+    //   setUserJson(typeJson);
+    // };
 
-      if (!tempEncouragementMessage.data && tempEncouragementMessage.message) {
-        console.error(tempEncouragementMessage.message);
-        toast.error(
-          "Failed to load encouragement message: " +
-            tempEncouragementMessage.message
-        );
+    const getEncouragementMessage = async () => {
+      const message = await generateMorningPracticeMessage();
+      // const message = { data: "Message will go here... " };
+      if (message.data) {
+        setEncouragementMessage(message.data);
       } else {
-        setEncouragementMessage(tempEncouragementMessage.data);
+        console.log("Error fetching encouragement message");
+        setEncouragementMessage("");
       }
     };
     getEncouragementMessage();
+    //  getUserJson();
   }, []);
 
   if (formsComplete) {
@@ -67,62 +48,36 @@ const MorningPractice = () => {
         <h1 className="text-primary text-2xl mb-7">
           Morning meditation unlocked!
         </h1>
-        <Meditation type="This is a meditation done in the morning to prepare the user for the day ahead" />
+        <Meditation type="This is a meditation done in the morning to prepare the user for the day" />
       </>
     );
   }
 
   return (
-    <div className="grid grid-rows-[auto,1fr,auto] items-center">
-      <div className="max-w-2xl">
+    <div className="grid grid-rows-[auto] max-w-2xl">
+      <div>
         <div className="flex items-center mb-3">
-          <FaSun className="text-yellow-500 text-2xl" />
-          <h1 className="text-primary text-2xl ml-1">
-            Morning Practice: Complete To Unlock Meditation!
-          </h1>
+          <FaMoon className="text-white-500 text-2xl" />
+          <h1 className="text-primary text-2xl ml-1">Morning Practice</h1>
         </div>
         {encouragementMessage === null ? (
-          <span className="loading loading-spinner loading-lg"></span>
+          <span className="loading loading-spinner loading-lg my-8"></span>
         ) : (
           <div
             className="my-8 text-secondary prose prose-slate max-w-none text-xl"
             dangerouslySetInnerHTML={{ __html: encouragementMessage }}
           />
         )}
-      </div>
-      <div className="max-w-2xl flex gap-5 mt-8">
-        <div className="w-1/2">
-          <h1 className="text-primary text-xl mb-3">
-            What are you grateful for today? What is going well? What makes you
-            happy?
-          </h1>
-          <FormContainer
-            action={updateMorningJournal}
-            onComplete={setGratitudeComplete}
-          >
-            <DailyInputForm title="grateful for" inputs={gratitudeItems} />
-          </FormContainer>
-          {gratitudeComplete && (
-            <FaCheckCircle className="text-green-500 text-2xl" />
-          )}
-        </div>
-        <div className="w-1/2">
-          <h1 className="text-primary text-xl mb-3">
-            What do you want to achieve today? Or how do you want to feel? Be
-            creative and think big!
-          </h1>
-          <FormContainer
-            action={updateMorningJournal}
-            onComplete={setToDoComplete}
-          >
-            <DailyInputForm inputs={toDoItems} />
-          </FormContainer>
-          {toDoComplete && (
-            <FaCheckCircle className="text-green-500 text-2xl" />
-          )}
-        </div>
+        <FormContainer
+          action={insertDiaryEntry}
+          className="flex w-full items-center"
+          onComplete={setJournalComplete}
+        >
+          <DiaryInputV2 words={100} type="morning" />
+        </FormContainer>
       </div>
     </div>
   );
 };
-export default MorningPractice;
+
+export default EveningPracticePage;
