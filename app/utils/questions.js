3d489186851d6aad3df610ab05dbac79484c2d16
Jake <jake@alkalimedia.co.uk>
2025-10-09 09:14:37 +0700

    Fixing spelling in questions

--- app/utils/questions.js
+++ app/utils/questions.js
@@ -9,18 +9,18 @@ export const questions = [
     step: 2,
     title: 'skills and achievements',
     content:
-      'Tell me about your skills and achievements. These can be from education, work, hobbies, overcoming life difficulties and so on. What gifts do you have, what nice things to other people say about you? What makes you special? What challenges have you overcome?',
+      'Tell me about your skills and achievements. These can be from education, work, hobbies, overcoming life difficulties, and so on. What gifts do you have? What nice things do other people say about you? What makes you special? What challenges have you overcome?',
   },
   {
     step: 3,
     title: 'obstacles and challenges',
     content:
-      "Tell me about the obstacles and challenges you are facing. What is holding you back? What are the things you should be doing that you don't do, and the things you shouldn't be doing that you should do? For each obstacle / challenge, tell me how you will feel when it is resolved. ",
+      "Tell me about the obstacles and challenges you are facing. What is holding you back? What are the things you should be doing that you don't do, and the things you shouldn't be doing that you do? For each obstacle or challenge, tell me how you will feel when it is resolved.",
   },
   {
     step: 4,
     title: 'All done!',
     content:
-      "That's great, I have all the info I need. You can now start the daily meditation and journallying exercises. It is recommended to restart this journalling process at least once a month as your goals to develop and change.",
+      "That's great, I have all the info I need. You can now start the daily meditation and journaling exercises. It is recommended to restart this journaling process at least once a month as your goals develop and change.",
   },
 ]
